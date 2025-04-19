eaa1900d3dbc9884cb79053dcaffaddf66e43ce3
Geeks_U <cncdre11@outlook.com>
2025-04-19 13:04:17 +0800

    用户页_组件绘制&骨架屏完成

---
+++ src/pages/my/components/PageSkeleton.vue
@@ -0,0 +1,247 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <scroll-view scroll-y="true" class="scroll-content scroll-content data-v-276ac604 data-v-276ac604" lower-threshold="50" enable-back-to-top="true">
+      <view class="custom-list custom-list data-v-276ac604 data-v-276ac604" style="padding-top:47px;">
+        <view class="row-container row-container data-v-276ac604 data-v-276ac604">
+          <view class="avatar-wrapper avatar-wrapper data-v-276ac604 data-v-276ac604">
+            <image class="avatar data-v-276ac604 sk-image" mode="scaleToFill"></image>
+          </view>
+          <view class="body-text body-text data-v-276ac604 data-v-276ac604">
+            <view class="body-title data-v-276ac604 sk-transparent sk-text-14-2857-277 sk-text">未登录</view>
+            <view class="body-subtitle data-v-276ac604 sk-transparent sk-text-14-2857-400 sk-text">点击登录账号</view>
+          </view>
+          <view class="footer-text footer-text data-v-276ac604 data-v-276ac604">
+            <text class="footer-label data-v-276ac604 sk-transparent sk-text-14-2857-162 sk-text">设置</text>
+          </view>
+        </view>
+      </view>
+      <view class="order-container order-container data-v-276ac604 data-v-276ac604">
+        <view class="top-row top-row data-v-276ac604 data-v-276ac604">
+          <text class="order-title data-v-276ac604 sk-transparent sk-text-14-2857-21 sk-text">我的订单</text>
+          <view class="right-container right-container data-v-276ac604 data-v-276ac604">
+            <text class="order-view-all data-v-276ac604 sk-transparent sk-text-14-2857-413 sk-text">查看全部订单</text>
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-right icons--uniui-right sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 17px;"></text>
+            </view>
+          </view>
+        </view>
+        <view class="body-row body-row data-v-276ac604 data-v-276ac604">
+          <view class="body-item body-item data-v-276ac604 data-v-276ac604">
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-shop icons--uniui-shop sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 24px;"></text>
+            </view>
+            <view class="data-v-276ac604 sk-transparent sk-text-14-2857-667 sk-text">待付款</view>
+          </view>
+          <view class="body-item body-item data-v-276ac604 data-v-276ac604">
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-shop-filled icons--uniui-shop-filled sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 24px;"></text>
+            </view>
+            <view class="data-v-276ac604 sk-transparent sk-text-14-2857-82 sk-text">待发货</view>
+          </view>
+          <view class="body-item body-item data-v-276ac604 data-v-276ac604">
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-paperplane icons--uniui-paperplane sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 24px;"></text>
+            </view>
+            <view class="data-v-276ac604 sk-transparent sk-text-14-2857-759 sk-text">待收货</view>
+          </view>
+          <view class="body-item body-item data-v-276ac604 data-v-276ac604">
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-chatboxes icons--uniui-chatboxes sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 24px;"></text>
+            </view>
+            <view class="data-v-276ac604 sk-transparent sk-text-14-2857-229 sk-text">待评价</view>
+          </view>
+          <view class="body-item body-item data-v-276ac604 data-v-276ac604">
+            <view is="node-modules/@dcloudio/uni-ui/lib/uni-icons/uni-icons" class="data-v-276ac604 data-v-276ac604">
+              <text class="uni-icons icons--uni-icons uniui-headphones icons--uniui-headphones sk-pseudo sk-pseudo-circle" style="color: #333333; font-size: 24px;"></text>
+            </view>
+            <view class="data-v-276ac604 sk-transparent sk-text-14-2857-617 sk-text">售后</view>
+          </view>
+        </view>
+      </view>
+      <view class="data-v-276ac604 data-v-276ac604">
+        <view class="guess-like-title data-v-276ac604 sk-transparent sk-text-14-2857-527 sk-text" style="background-position-x: 50%;">猜你喜欢</view>
+        <view is="pages/my/components/guessLike" class="data-v-276ac604 data-v-276ac604">
+          <view class="product-list guessLike--product-list data-v-8b991335 guessLike--data-v-8b991335">
+            <view class="product-item guessLike--product-item data-v-8b991335 guessLike--data-v-8b991335">
+              <view is="components/ProductCard" class="data-v-8b991335 guessLike--data-v-8b991335">
+                <view class="card ProductCard--card data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                  <image class="image ProductCard--image data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-image" mode="aspectFill"></image>
+                  <view class="content ProductCard--content data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                    <view class="title ProductCard--title data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-438 sk-text">平台有道翻译王...</view>
+                    <view class="desc ProductCard--desc data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-812 sk-text">差旅学习必备，...</view>
+                    <view class="price-box ProductCard--price-box data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                      <text class="origin-price ProductCard--origin-price data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-739 sk-text">￥998.00</text>
+                      <text class="order-num ProductCard--order-num data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-625 sk-text">23人已购</text>
+                    </view>
+                  </view>
+                </view>
+              </view>
+            </view>
+            <view class="product-item guessLike--product-item data-v-8b991335 guessLike--data-v-8b991335">
+              <view is="components/ProductCard" class="data-v-8b991335 guessLike--data-v-8b991335">
+                <view class="card ProductCard--card data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                  <image class="image ProductCard--image data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-image" mode="aspectFill"></image>
+                  <view class="content ProductCard--content data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                    <view class="title ProductCard--title data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-303 sk-text">手工八年老陈醋...</view>
+                    <view class="desc ProductCard--desc data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-225 sk-text">地道醇香，醋酸...</view>
+                    <view class="price-box ProductCard--price-box data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                      <text class="origin-price ProductCard--origin-price data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-120 sk-text">￥30.00</text>
+                      <text class="order-num ProductCard--order-num data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-transparent sk-text-14-2857-951 sk-text">19人已购</text>
+                    </view>
+                  </view>
+                </view>
+              </view>
+            </view>
+            <view class="product-item guessLike--product-item data-v-8b991335 guessLike--data-v-8b991335">
+              <view is="components/ProductCard" class="data-v-8b991335 guessLike--data-v-8b991335">
+                <view class="card ProductCard--card data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                  <image class="image ProductCard--image data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-image" mode="aspectFill"></image>
+                </view>
+              </view>
+            </view>
+            <view class="product-item guessLike--product-item data-v-8b991335 guessLike--data-v-8b991335">
+              <view is="components/ProductCard" class="data-v-8b991335 guessLike--data-v-8b991335">
+                <view class="card ProductCard--card data-v-a4cce9c2 ProductCard--data-v-a4cce9c2">
+                  <image class="image ProductCard--image data-v-a4cce9c2 ProductCard--data-v-a4cce9c2 sk-image" mode="aspectFill"></image>
+                </view>
+              </view>
+            </view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
+</template>
+
+<style scoped>
+.sk-transparent {
+    color: transparent !important;
+  }
+.sk-text-14-2857-277 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 48.4615rpx;
+    position: relative !important;
+  }
+.sk-text {
+    background-origin: content-box !important;
+    background-clip: content-box !important;
+    background-color: transparent !important;
+    color: transparent !important;
+    background-repeat: repeat-y !important;
+  }
+.sk-text-14-2857-400 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 37.6923rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-162 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-21 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 45.7692rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-413 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 37.6923rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-667 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-82 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-759 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-229 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-617 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-527 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 53.8462rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-438 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-812 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 32.3077rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-739 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 40.3846rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-625 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 29.6154rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-303 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 43.0769rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-225 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 32.3077rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-120 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 40.3846rpx;
+    position: relative !important;
+  }
+.sk-text-14-2857-951 {
+    background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+    background-size: 100% 29.6154rpx;
+    position: relative !important;
+  }
+.sk-image {
+    background: #EFEFEF !important;
+  }
+.sk-pseudo::before, .sk-pseudo::after {
+      background: #EFEFEF !important;
+      background-image: none !important;
+      color: transparent !important;
+      border-color: transparent !important;
+    }
+.sk-pseudo-rect::before, .sk-pseudo-rect::after {
+      border-radius: 0 !important;
+    }
+.sk-pseudo-circle::before, .sk-pseudo-circle::after {
+      border-radius: 50% !important;
+    }
+.sk-container {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: transparent;
+  }
+
+</style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,27 +1,313 @@
+<template>
+  <!-- 需要显式给出高度才能触发scroll -->
+  <scroll-view
+    v-if="!skeletonLoading"
+    scroll-y
+    class="scroll-content"
+    @scrolltolower="handleScrollToLower"
+    :lower-threshold="50"
+  >
+    <!-- 用户信息 -->
+     <!-- 未登录 -->
+    <view v-if="true" class="custom-list" :style="navBarStyle">
+      <view class="row-container">
+        <view class="avatar-wrapper">
+          <image
+            class="avatar"
+            src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+            mode="scaleToFill"
+          />
+        </view>
+        <view class="body-text">
+          <p class="body-title">未登录</p>
+          <p class="body-subtitle">点击登录账号</p>
+        </view>
+        <view class="footer-text">
+          <text class="footer-label">设置</text>
+        </view>
+      </view>
+    </view>
+    <!-- 已登录 -->
+    <view v-else class="custom-list" :style="navBarStyle">
+      <view class="row-container">
+        <view class="avatar-wrapper">
+          <image
+            class="avatar"
+            src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+            mode="scaleToFill"
+          />
+        </view>
+        <view class="body-text">
+          <p class="body-title">昵称</p>
+          <p class="body-subtitle">更新个人信息</p>
+        </view>
+        <view class="footer-text">
+          <text class="footer-label">设置</text>
+        </view>
+      </view>
+    </view>
+
+    <!-- 订单模块 -->
+    <view class="order-container">
+      <view class="top-row">
+        <text class="order-title">我的订单</text>
+        <view class="right-container">
+          <text class="order-view-all">查看全部订单</text>
+          <uni-icons type="right" size="17" />
+        </view>
+      </view>
+
+      <view class="body-row">
+        <view class="body-item">
+          <uni-icons type="shop" size="24" />
+          <p>待付款</p>
+        </view>
+        <view class="body-item">
+          <uni-icons type="shop-filled" size="24" />
+          <p>待发货</p>
+        </view>
+        <view class="body-item">
+          <uni-icons type="paperplane" size="24" />
+          <p>待收货</p>
+        </view>
+        <view class="body-item">
+          <uni-icons type="chatboxes" size="24" />
+          <p>待评价</p>
+        </view>
+        <view class="body-item">
+          <uni-icons type="headphones" size="24" />
+          <p>售后</p>
+        </view>
+      </view>
+    </view>
+
+    <!-- 猜你喜欢 -->
+    <view>
+      <p class="guess-like-title">猜你喜欢</p>
+      <guessLike :guessLikeList="guessLikeList" />
+    </view>
+
+    <!-- 加载提示 -->
+    <view v-if="isLoading" style="text-align:center; padding: 10px;">加载中...</view>
+    <view v-if="isFinished" style="text-align:center; padding: 10px;">没有更多了</view>
+  </scroll-view>
+  <PageSkeleton v-else></PageSkeleton>
+
+</template>
+
 <script lang="ts" setup>
-import { useUserStore } from '@/stores/index'
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
 
-const userStore = useUserStore();
+// 组件引入
+import guessLike from './components/guessLike.vue'
+import PageSkeleton from './components/PageSkeleton.vue'
 
-import {  http } from '@/utils/http'
+// 接口
+import { getHomeGuessLikeData } from '@/services/home'
 
-const getData = () => {
-  http<any[]>({
-    method: 'GET',
-    url: '/home/banner',
-  }).then((res) => {
-    console.log('Data fetched successfully:', res);
-  }).catch((error) => {
-    console.error('Error fetching data:', error);
-  });
+// 数据
+const { safeAreaInsets, statusBarHeight } = uni.getSystemInfoSync()
+const safeTop = statusBarHeight || safeAreaInsets?.top || 20
+const navBarStyle = {
+  paddingTop: safeTop + 'px'
+}
+
+const guessLikeList = ref([])
+
+const guessLikeListPageInfo = ref({
+  currentPage: 0,
+  pageSize: 10,
+  pageSum: 0
+})
+
+const isLoading = ref(false)
+const isFinished = ref(false)
+const skeletonLoading = ref(true)
+
+const fetchGuessLikeList = async () => {
+  try {
+    guessLikeListPageInfo.value.currentPage += 1
+    isFinished.value = false
+    const res = await getHomeGuessLikeData(
+      guessLikeListPageInfo.value.currentPage,
+      guessLikeListPageInfo.value.pageSize
+    )
+    guessLikeListPageInfo.value.pageSum = res.result.pages
+
+    guessLikeList.value = res.result.items.map((item) => ({
+      id: item.id,
+      name: item.name,
+      desc: item.desc,
+      picture: item.picture,
+      price: item.price,
+      orderNum: item.orderNum
+    }))
+
+    if (guessLikeListPageInfo.value.currentPage >= guessLikeListPageInfo.value.pageSum)
+      isFinished.value = true
+  } catch (error) {
+    console.error('获取猜你喜欢数据失败:', error)
+  }
+}
+
+// 页面加载时获取数据
+onLoad(async () => {
+  await Promise.all([
+  fetchGuessLikeList()
+  ])
+  skeletonLoading.value = false
+})
+
+const loadMoreGuessLike = async () => {
+  if (isFinished.value) {
+    setTimeout(() => {
+      isFinished.value = false
+    }, 3000)
+    return
+  }
+  if (isLoading.value) return
+
+  isLoading.value = true
+  try {
+    const nextPage = guessLikeListPageInfo.value.currentPage + 1
+    const res = await getHomeGuessLikeData(nextPage, guessLikeListPageInfo.value.pageSize)
+
+    const newItems = res.result.items.map((item) => ({
+      id: item.id,
+      name: item.name,
+      desc: item.desc,
+      picture: item.picture,
+      price: item.price,
+      orderNum: item.orderNum
+    }))
+
+    guessLikeList.value.push(...newItems)
+    guessLikeListPageInfo.value.currentPage = nextPage
+    guessLikeListPageInfo.value.pageSum = res.result.pages
+
+    if (nextPage >= guessLikeListPageInfo.value.pageSum) {
+      isFinished.value = true
+    }
+  } catch (error) {
+    console.error('加载更多猜你喜欢失败:', error)
+  } finally {
+    isLoading.value = false
+  }
+}
+
+const handleScrollToLower = () => {
+  loadMoreGuessLike()
 }
 </script>
 
-<template>
-  <div>
-    <p v-if="userStore.userInfo.isLoggedIn">Hello, {{ userStore.userInfo.name }}</p>
-    <button @tap="userStore.login('Tom', 'hhh')">Login</button>
-    <button @tap="userStore.logout">Logout</button>
-    <button @tap="getData">Get Data</button>
-  </div>
-</template>
+<style scoped>
+.scroll-content {
+  height: 100vh;
+}
+
+.custom-list {
+  background-color: #c99999;
+  color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.row-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.avatar-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+}
+
+.avatar {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.body-text {
+  text-align: left;
+  flex-grow: 1;
+}
+
+.body-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 4px;
+}
+
+.body-subtitle {
+  font-size: 14px;
+  color: #fff;
+}
+
+.footer-text {
+  margin-top: auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.footer-label {
+  font-size: 16px;
+  color: #fff;
+}
+
+.top-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.order-title {
+  font-size: 17px;
+  font-weight: bold;
+}
+
+.order-view-all {
+  font-size: 14px;
+  color: #999;
+}
+
+.right-container {
+  display: flex;
+  align-items: center;
+}
+
+.body-row {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.body-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+}
+
+.order-container {
+  border: 2px solid #c99999;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 10px 0;
+}
+
+.guess-like-title {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 20px 0;
+}
+</style>
